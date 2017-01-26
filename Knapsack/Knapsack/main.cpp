377af75440a69411513d5d42557779b4d0268786
jontelarsson94 <jonathan-larsson@outlook.com>
2017-01-26 22:15:15 +0100

    start of knapsack

---
+++ Knapsack/Knapsack/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Knapsack
+//
+//  Created by Jonathan Larsson on 2017-01-26.
+//  Copyright © 2017 Jonathan Larsson. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    float val[] = {20, 40, 50, 36, 25, 64, 54, 18, 46, 28};
+    float wt[] = {15, 32, 60, 80, 43, 120, 77, 6, 93, 35};
+    int W = 420;
+    int n = sizeof(val)/sizeof(int);
+    
+    cout << n;
+    
+    return 0;
+}
