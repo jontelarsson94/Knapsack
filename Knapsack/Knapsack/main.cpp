5efd7c839b64d12bba506ef01bc8e543d89f1e6b
jontelarsson94 <jonathan-larsson@outlook.com>
2017-01-26 23:16:06 +0100

    sortbyratio

--- Knapsack/Knapsack/main.cpp
+++ Knapsack/Knapsack/main.cpp
@@ -9,15 +9,44 @@
 #include <iostream>
 using namespace std;
 
+void sortByRatio(float benefit[], float weight[], float ratio[], int size)
+{
+    
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            if(ratio[i] > ratio[j]) {
+                float save = ratio[j];
+                ratio[j] = ratio[i];
+                ratio[i] = save;
+                save = benefit[j];
+                benefit[j] = benefit[i];
+                benefit[i] = save;
+                save = weight[j];
+                weight[j] = weight[i];
+                weight[i] = save;
+            }
+        }
+    }
+}
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     
-    float val[] = {20, 40, 50, 36, 25, 64, 54, 18, 46, 28};
-    float wt[] = {15, 32, 60, 80, 43, 120, 77, 6, 93, 35};
+    float benefit[] = {20, 40, 50, 36, 25, 64, 54, 18, 46, 28};
+    float weight[] = {15, 32, 60, 80, 43, 120, 77, 6, 93, 35};
     int W = 420;
-    int n = sizeof(val)/sizeof(int);
+    int n = sizeof(benefit)/sizeof(float);
+    float ratio[n];
     
-    cout << n;
+    for(int i = 0; i < n; i++) {
+        ratio[i] = benefit[i] / weight[i];
+    }
+    
+    sortByRatio(benefit, weight, ratio, n);
+    
+    for(int i = 0; i < 10; i++) {
+        cout << ratio[i] << endl;
+    }
     
     return 0;
 }
